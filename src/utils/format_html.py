a308ecaedffb380245255fe48edaf7f9a444b325
Omar <sup4anski@gmail.com>
2025-09-18 17:14:45 +0500

    better map attr and some fix

--- src/utils/format_html.py
+++ src/utils/format_html.py
@@ -66,5 +66,5 @@ def extract_product_properties_from_html(html: str) -> str:
 
     # Соединяем в одну строку
 
-    logger.debug(f"Product extracted from html properties: {details}")
+    logger.info(f"Product extracted from html properties: {details}")
     return " ".join(details)
